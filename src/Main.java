a7404074c36e7015123cf4767ab4947e499eee74
GinkoGintoki <mrhuman00@gmail.com>
2021-04-24 13:42:45 +0600

    RomanToInteger.java - Convert Roman numerals into integer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
+/*        Symbol       Value
+        I             1
+        V             5
+        X             10
+        L             50
+        C             100
+        D             500
+        M             1000*/
+        RomanToInteger romanToInteger = new RomanToInteger();
+        System.out.println(romanToInteger.romanToInt("IV"));
     }
+
 }
