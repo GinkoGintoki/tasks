a1367aae64c0f183a96ceac53593e66b1e9c3740
GinkoGintoki <mrhuman00@gmail.com>
2021-04-24 16:48:58 +0600

    LongestCommonPrefix.java - Function that finding the longest common prefix string amongst an array of strings

---
+++ src/task/LongestCommonPrefix.java
@@ -0,0 +1,16 @@
+package task;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        char[] prefix = strs[0].toCharArray();
+        int max = prefix.length;
+        for (int i = 1; i < strs.length; i++) {
+            int idx = 0;
+            char[] curr = strs[i].toCharArray();
+            while (idx < Math.min(max, curr.length) && prefix[idx] == curr[idx])
+                idx++;
+            max = idx;
+        }
+        return strs[0].substring(0, max);
+    }
+}
